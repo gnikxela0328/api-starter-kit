9c0ede3fe98320fe85d957ddcef632a9cde03763
Alex <purpletsar@icloud.com>
2022-12-01 12:03:25 -0600

    env example added and dockerfile moved

--- server/api/keys.py
+++ server/api/keys.py
@@ -1,7 +1,9 @@
-JWT_SECRET = "jwtsecretpass"
+import os
+
+JWT_SECRET = os.environ["JWT_SECRET"]
 
 DB_CONFIG = {
-    'DB_USER': 'starter',
-    'DB_PASS': 'password',
-    'DB_DOMAIN': 'db:5432/starterkit' #add db name to end
+    'DB_USER': os.environ["DB_USER"],
+    'DB_PASS': os.environ["DB_PASS"],
+    'DB_DOMAIN': os.environ["DB_DOMAIN"]
 }
